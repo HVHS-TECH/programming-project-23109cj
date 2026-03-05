b5769ef6afea3df6caaa31b2962d3585c71522bb
23109cj <23109cj@hvhs.school.nz>
2026-03-06 09:59:24 +1300

    still needs work

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log("script.js")
 
 let throttle = 0.5;
 let pitch = 0;
-let verticalSpeed = 0;
-let horizontalSpeed = 0;
+
 const GRAVITY = 5;
 const LIFTCOEFFICENT =0.3;
+const DRAG = 6;
 
 //plane source = https://images.fineartamerica.com/images-medium-large-5/2-illustration-of-an-a-7e-corsair-ii-inkworm.jpg
 
@@ -16,74 +16,94 @@ function preload() {
 }
 
 function setup(){
-    world.gravity.y = 10;
+
+    frameRate(10)
+
     cnv = new Canvas(windowWidth, windowHeight);
     const GROUND_HEIGHT = windowHeight - windowHeight/6;
     angleMode(DEGREES);
 
-    plane = new Sprite(windowWidth/6, 600, 40, 20, 'd');
+    plane = new Sprite(windowWidth/6, GROUND_HEIGHT, 20, 20, 'd');
     plane.image = (imgPlane);
     plane.image.scale.y = 0.1;
-    plane.image.scale.x = -0.1; 
+    plane.image.scale.x = -0.1;
+    plane.drag = DRAG; 
+    plane.bouncieness = 0;
 
-    
-    //plane.vel.x=10
-    //plane.vel.y=-1
     ground = new Sprite(windowWidth/2, GROUND_HEIGHT/3 *4.35, windowWidth, GROUND_HEIGHT, 'k');
     ground.color = '#00ff00';
+    ground.bouncieness = 0;
+
 }
 
 
-
-
-
-function draw(){
-    background('#0000ff');
-
-
-    //--------------------------------------------
+//--------------------------------------------
     //Take keyboard input
     //--------------------------------------------
-    if(kb.pressing ('w')){
-        if(throttle <= 60){
-            throttle = throttle * 1.1;
+function keyboardInput(){
+        if(kb.pressing ('w')){
+            if(throttle <= 60){
+                throttle = throttle * 1.1;
+            }
         }
+
+        if(kb.pressing ('s')){
+            if(throttle >= 0.1){
+                throttle = throttle * 0.99;
+            }
+        }
+
+        if(kb.pressing ('a')){
+            if(pitch <= -65){
+                pitch = pitch;
+            } else{
+                    pitch = pitch - (1 * Math.sqrt(throttle));
+            }
+        }
+
+        if(kb.pressing ('d')){
+            if(pitch >= 85){
+                pitch = pitch;
+            } else{
+                    pitch = pitch + (1 * Math.sqrt(throttle))
+            }
+        }
+        return pitch, throttle;
     }
 
-    if(kb.pressing ('s')){
-        if(throttle >= 0.1){
-            throttle = throttle * 0.99;
-        }
-    }
 
-    if(kb.pressing ('a')){
-        if(pitch <= -65){
-            pitch = pitch;
-        } else{
-                pitch = pitch - (1 * Math.sqrt(throttle));
-        }
-    }
+//--------------------------------------------
+//calculate vertical and horizontal speeds based on pitch and throttle - vetical speed add gravity aswell
+//--------------------------------------------
 
-    if(kb.pressing ('d')){
-        if(pitch >= 85){
-            pitch = pitch;
-        } else{
-                pitch = pitch + (1 * Math.sqrt(throttle))
-        }
-    }
-
-    //--------------------------------------------
-    //calculate vertical and horizontal speeds based on pitch and throttle - vetical speed add gravity aswell
-    //--------------------------------------------
-    horizontalSpeed = Math.abs( throttle);
+function calculateVelocityVectors(_speed, _angle, _liftOfObject){
+    horizontalSpeed = Math.abs( _speed * (Math.cos(_angle) - Math.cos(90 - _angle) * _liftOfObject));
+    verticalSpeed = (-1 * _speed * (Math.sin(_angle) + Math.sin(90 - _angle) * _liftOfObject)) + GRAVITY;
+    return horizontalSpeed, verticalSpeed;
+}
     
-    verticalSpeed = verticalSpeed - (LIFTCOEFFICENT * throttle * Math.sin(pitch))  + GRAVITY;
+function draw(){
+    let verticalSpeed = 0;
+    let horizontalSpeed = 0;
+    background('#0000ff');
+
+    keyboardInput()    
+    calculateVelocityVectors(throttle,pitch,LIFTCOEFFICENT)
+
+    //--------------------------------------------
+    //move camera & ground
+    //--------------------------------------------
+        camera.moveTo(plane.x + 2/6 * windowWidth, plane.y, 12);
+        //ground.x = (camera.x);
+
 
     //--------------------------------------------
     //Apply rotation and movement to the plane
     //--------------------------------------------
-    plane.rotation = pitch;
-    plane.vel.x = horizontalSpeed;
-    plane.vel.y = verticalSpeed;
+        plane.rotation = pitch;
+        plane.vel.x = horizontalSpeed;
+        plane.vel.y = verticalSpeed;
+        console.log(plane.x)
+        console.log(plane.y)
 
 }
