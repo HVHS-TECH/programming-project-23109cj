355621d3e5754259685462a3cfa71198c1776388
23109cj <23109cj@hvhs.school.nz>
2026-03-13 09:57:50 +1300

    missiles :)

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ console.log("script.js")
 
 let throttle = 0.5;
 let pitch = 0;
+let cnv;
+let missileTimer = 0;
+let missile;
+let score = 0;
+let enemyTimer =500;
 
 const GRAVITY = 5;
 const LIFTCOEFFICENT =0.3025;
 const DRAG = 6;
 const FRAMERATE = 60;
 const PITCHSENSITIVITY = 0.12;
-let GROUND_HEIGHT; //as var so it can be read from all functions - value never cahnged after setup()
+let GROUND_HEIGHT;      //as var so it can be read from all functions - value never changed after setup() so I have named it as a constant
 
 
 //plane img source = https://images.fineartamerica.com/images-medium-large-5/2-illustration-of-an-a-7e-corsair-ii-inkworm.jpg
@@ -17,6 +22,7 @@ function preload() {
     console.log('preload()')
     imgPlane   = loadImage('A7.png');
     imgCloud   = loadImage('Cloud.png');
+    imgMissile = loadImage('missile.png');
     
 }
 
@@ -25,9 +31,13 @@ function setup(){
     frameRate(FRAMERATE)
     angleMode(DEGREES);
     GROUND_HEIGHT = windowHeight - windowHeight/6;
-    createClouds();
 
-    cnv = new Canvas(windowWidth -1, windowHeight-1);
+    cnv = new Canvas(windowWidth, windowHeight);
+
+
+    cloud = new Sprite(windowWidth, random(0,GROUND_HEIGHT), 10, 10, 'n')
+    cloud.image = (imgCloud);
+    cloud.image.scale = 0.5;
 
     plane = new Sprite(windowWidth/6, GROUND_HEIGHT, 20, 20, 'd');
     plane.image = (imgPlane);
@@ -45,27 +55,24 @@ function setup(){
 
 }
 
+//create enemies every 10 sec
+
+
+
+
+
+
+
 //--------------------------------------------------------
 //Launch Missle
 //--------------------------------------------------------
 function launchMissile(){
-    missile = new Sprite();
-    missile.img=()
-    missile.vel.x = calculateHorizontalVelocityVectors(throttle,pitch,LIFTCOEFFICENT)
+    missile = new Sprite(plane.x -20, plane.y - 10, 40,10,'k');
+    missile.vel.x = calculateHorizontalVelocityVectors(throttle,pitch,LIFTCOEFFICENT) *1.25
     missile.vel.y = calculateVerticalVelocityVectors(throttle,pitch,LIFTCOEFFICENT)
-    missile.point
-}
-
-
-//-------------------------------------------------------
-//create clouds
-//-------------------------------------------------------
-function createClouds(){
-    console.log('createClouds()')
-        cloud = new Sprite(windowWidth, random(0,GROUND_HEIGHT), 10, 10, 'n')
-        cloud.image = (imgCloud);
-        cloud.image.scale = 0.5;
-    
+    missile.image = (imgMissile)
+    missile.scale.x = -0.1;
+    missile.scale.y = 0.1;
 }
 
 //--------------------------------------------
@@ -97,15 +104,20 @@ function calculateVerticalVelocityVectors(_speed, _angle, _liftOfObject){
 //move camera & ground
 //--------------------------------------------
 function moveCameraAndGround(_percentperframe){
-    camera.x += (plane.x - camera.x) * (_percentperframe/100) //- (2/6 * windowWidth);
+    camera.x += ((plane.x - camera.x) * (_percentperframe/100)) + (1/14*windowWidth);
     camera.y += (plane.y - camera.y) * (_percentperframe/100);
     ground.x = camera .x; 
+    //console.log(camera.x);
+    //console.log(plane.x)
 }
 
 
 
 function draw(){
     background('#0000ff');
+    if(missileTimer >= 1){
+        missileTimer -= 1;
+    }
     
     //--------------------------------------------
     //Take keyboard input
@@ -121,6 +133,7 @@ function draw(){
             throttle = throttle * (1 - 0.1/6);
         }
     }
+
     if(plane.colliding(ground) == false){
         if(kb.pressing ('a')){
             if(pitch <= -65){
@@ -142,20 +155,47 @@ function draw(){
         plane.rotation = 0;
     }
 
+    if(kb.presses('space') && missileTimer ==0){
+        missileTimer = 300;
+        launchMissile()
+        console.log(mouseX,mouseY)
+    }
 
-    //
+    
+//missile movement & removal after time
+    if(missileTimer != 0){
+        console.log('timer not=0')
+        missile.moveTowards(mouse.x, mouse.y, 0.55)
+    }
+
+    if(missileTimer == 1){
+        missile.remove();
+    }
+
+
+
+    //----------------------------------------------
+    //move clouds accros screen
+
     if(cloud.x < camera.x - windowWidth/2){
         console.log('cloudIf')
         cloud.x = camera.x + windowWidth/2;
     }
     
-    moveCameraAndGround(20)
 
+    //------------------------
+    //cloud velocity 
+    //------------------------
+    cloud.vel.x = plane.vel.x/3;
+
+
+
+    moveCameraAndGround(20)
     //--------------------------------------------
     //Apply rotation and movement to the plane
     //--------------------------------------------
-        plane.rotation = pitch;
-        plane.vel.x = calculateHorizontalVelocityVectors(throttle,pitch,LIFTCOEFFICENT);
-        plane.vel.y = calculateVerticalVelocityVectors(throttle,pitch,LIFTCOEFFICENT);
+    plane.rotation = pitch;
+    plane.vel.x = calculateHorizontalVelocityVectors(throttle,pitch,LIFTCOEFFICENT);
+    plane.vel.y = calculateVerticalVelocityVectors(throttle,pitch,LIFTCOEFFICENT);
 
 }
